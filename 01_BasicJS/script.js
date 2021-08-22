a9c99a90ca72a7394ccc48a187d611bf658598eb
Nichakan Sathaphon <nichakan4740@gmail.com>
2021-08-22 22:49:28 +0700

    add Thai ver

--- 01_BasicJS/script.js
+++ 01_BasicJS/script.js
@@ -1,13 +1,13 @@
-//variables, funtion names, and operators are all case sensitive
-//Statements in ECMAScript are terminated by a semicolon
-//Even semicolon is not required but not recommended to omit the semicolon
+//variables, funtion names, and operators are all case sensitive  //ตัวแปร ชื่อฟังก์ชัน และตัวดำเนินการทั้งหมดคำนึงถึงตัวพิมพ์เล็กและตัวพิมพ์ใหญ่
+//Statements in ECMAScript are terminated by a semicolon //คำสั่งใน ECMAScript สิ้นสุดลงด้วยเครื่องหมายอัฒภาค
+//Even semicolon is not required but not recommended to omit the semicolon //ไม่จำเป็นต้องใช้เครื่องหมายอัฒภาคแต่ไม่แนะนำให้ข้ามเครื่องหมายอัฒภาค
 
 let num = 5;
 let Num = 'This is not a number';
 console.log('num is ' + num);
 console.log('Num is ' + Num);
 
-//Multiple statements can be combined into a code block by using {}
+//Multiple statements can be combined into a code block by using {}//คำสั่งหลายคำสั่งสามารถรวมกันเป็นบล็อคโค้ดได้โดยใช้ {}
 //require code block only when executing multiple statements.
 
 if (num == 5) {
@@ -52,5 +52,6 @@ let myObj = [
 ];
 
 console.log(`${JSON.stringify(myObj)}`);
-//The JSON.stringify() method converts a JavaScript object or value to a JSON string
+//The JSON.stringify() method converts a JavaScript object or value to a JSON string 
+//The JSON.stringify() วิธีการแปลงวัตถุ JavaScript หรือค่าเป็นสตริง JSON
 console.log(`type of myObj is ${typeof myObj}`);
