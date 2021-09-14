714cc09531ff0b2328ecf4c3ee852b915c130ea3
Nichakan Sathaphon <nichakan4740@gmail.com>
2021-09-14 12:11:35 +0700

    add

--- 14-09/script.js
+++ 14-09/script.js
@@ -4,7 +4,7 @@ let per1 = { id: 1, name: 'Joe' };
 //store memory address that points to object properties
 let per2 = per1;
 
-doSomething(per2); //hoisting, use only var scope
+/*doSomething(per2); //hoisting, use only var scope
 function doSomething(p) {
     //p=per2  (memory address of per2 to p)
     p.name = 'Mary'; //pers2.name='Mary'
@@ -27,3 +27,35 @@ function doSomething(num) {
 
 console.log(num1); //num1=10
 console.log(num2); //num2=10
+
+
+
+// Aray
+let arr1 = [];
+console.log(typeof arr1);
+arr1 = [5, 2, 10, 'student']; //untyped & unsized
+console.log(arr1.length);
+console.log(arr1[0]);
+console.log(arr1[arr1.length - 1]);
+console.log(   arr1[ arr1[1] ]     );
+*/
+
+//array of objects
+
+let arr2 = [
+    { id: 1, name: 'Adam' },
+    { id: 2, name: 'Joe' }
+  ];
+  // console.log(arr2[1].name);
+
+  
+  let arr3 = [
+    // [(0,0) 1, (0,1) 3, (0,2) 5, (0,3) 7],
+    // [(1,0)2, (1,1) 4, (1,2)6,  (1,3) 8]
+    [1, 3, 5, 7],
+    [2, 4, 6, 8]
+  ];
+  // console.log(arr3[1][1]);
+  
+  let arr4 = [...arr3, 0.5, 100, ...arr2];
+  console.log(arr4);
