b44ddf78df7fcc46102992116a1830f5dc3965fb
Nichakan Sathaphon <nichakan4740@gmail.com>
2021-09-14 12:41:45 +0700

    add

--- 14-09/script.js
+++ 14-09/script.js
@@ -48,14 +48,14 @@ let arr2 = [
   ];
   // console.log(arr2[1].name);
 
-  
+
   let arr3 = [
     // [(0,0) 1, (0,1) 3, (0,2) 5, (0,3) 7],
     // [(1,0)2, (1,1) 4, (1,2)6,  (1,3) 8]
     [1, 3, 5, 7],
     [2, 4, 6, 8]
   ];
-  // console.log(arr3[1][1]);
+  console.log(arr3[1][1]);
   
   let arr4 = [...arr3, 0.5, 100, ...arr2];
   console.log(arr4);
